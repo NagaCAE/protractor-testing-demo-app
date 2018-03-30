2ed0eb0fa9127cd5889ba1fb155942a9cd196b85
Vitalii Chmovzh <vitalii.chmovzh@gmail.com>
2018-03-30 15:27:15 -0400

    Updated specs names

--- e2e/login.e2e-spec.ts
+++ e2e/login.e2e-spec.ts
@@ -17,13 +17,13 @@ describe('Login page', () => {
     expect(page.signIn.getText()).toEqual('Sign in');
   });
 
-  it ('should display an error message to user if he provided incorrect credentials', () => {
+  it ('should display an error message to the user if they provided incorrect credentials', () => {
     page.trySignIn('123', '123');
     browser.wait(EC.visibilityOf(page.errorMessage));
     expect(page.errorMessage.getText()).toEqual('Incorrect username or password');
   });
 
-  it ('should redirect user to the dashboard page if he provided correct credentials', () => {
+  it ('should redirect the user to the dashboard page if they provided correct credentials', () => {
     const dashboardPage = new DashboardPage();
     page.trySignIn('correct', 'correct');
     browser.wait(EC.visibilityOf(dashboardPage.title));
